4daff108e53bf6dd625e0d8ce2b6eb22772025e4
Jai <jairevankar98@gmail.com>
2025-09-08 09:30:04 +0530

    Minor Changes

--- InWex_Frontend/src/pages/RegisterPage.jsx
+++ InWex_Frontend/src/pages/RegisterPage.jsx
@@ -9,14 +9,14 @@ const RegisterPage = () => {
         { name: 'fullname', label: 'Full Name', type: 'text', placeholder: 'Test Example' },
         { name: 'email', label: 'Email Address', type: 'email', placeholder: 'test@example.com' },
         { name: 'password', label: 'Password', type: 'password', placeholder: '••••••••' },
-        { name: 'contact', label: 'Contact', type: 'tel', placeholder: '+91 12345-67890', pattern: '[0-9]{10}' },
+        { name: 'contact_number', label: 'Contact', type: 'tel', placeholder: '+91 12345-67890', pattern: '[0-9]{10}' },
     ]
 
     const orgFields = [
         { name: 'orgName', label: 'Org Name', type: 'text', placeholder: 'Test Example' },
         { name: 'email', label: 'Email Address', type: 'email', placeholder: 'test@example.com' },
         { name: 'password', label: 'Password', type: 'password', placeholder: '••••••••' },
-        { name: 'contact', label: 'Contact', type: 'tel', placeholder: '+91 12345-67890', pattern: '[0-9]{10}' },
+        { name: 'contact_number', label: 'Contact', type: 'tel', placeholder: '+91 12345-67890', pattern: '[0-9]{10}' },
     ]
 
     return (
